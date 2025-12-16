ccaff174e898457e2d27d74a0f05686e355c10ae
Erick Nunes da Silva <149442208+erickknsilva@users.noreply.github.com>
2025-12-16 11:49:09 -0300

    Update SecurityConfig.java

--- src/main/java/com/erickWck/edge_service/config/SecurityConfig.java
+++ src/main/java/com/erickWck/edge_service/config/SecurityConfig.java
@@ -24,6 +24,7 @@ public class SecurityConfig {
                     exchange.pathMatchers(HttpMethod.POST, "/api/auth/login").permitAll()
                             .pathMatchers(HttpMethod.POST, "/bookings").permitAll()
                             .pathMatchers(HttpMethod.GET, "/api/auth/.well-known/jwks.json", "/api/flights/**").permitAll()
+                            .pathMatchers(HttpMethod.GET, "/actuator/**").permitAll()
                             .anyExchange().authenticated();
                 })
                 .oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()));
